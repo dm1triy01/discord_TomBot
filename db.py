84b1afc0b77c9e09abe62282f9fa6ed5314aab8e
Dm1triy <dmitriy.owl@mail.ru>
2022-07-30 02:13:04 +0300

    Minor fix.

--- db.py
+++ db.py
@@ -1,20 +1,22 @@
 import psycopg2
 
-
 # from itertools import chain
 # from datetime import date, datetime
 # import re
+
+import cfg
+
 
 # Database requests
 class db:
 
     def __init__(self):
         self.connection = psycopg2.connect(
-            database="",
-            user="postgres",
-            password="3400",
-            host="127.0.0.1",
-            port="5432"
+            database=cfg.database,
+            user=cfg.user,
+            password=cfg.password,
+            host=cfg.host,
+            port=cfg.port
         )
         self.cursor = self.connection.cursor()
 
@@ -28,28 +30,3 @@ class db:
         with self.connection:
             self.cursor.execute("")
             return 0
-
-    # def check_user(self, id):
-    #     with self.connection:
-    #         self.cursor.execute("select Score from [User] where Id = '" + str(id) + "'")
-    #         result = self.cursor.fetchall()
-    #         return bool(len(result))
-    #
-    # def add_user(self, id):
-    #     with self.connection:
-    #         return self.cursor.execute("insert into [User] values(" + str(id) + " ,1000, 10)")
-    #
-    # def get_score(self, id):
-    #     self.cursor.execute("select Score from [User] where Id = '" + str(id) + "'")
-    #     list = self.cursor.fetchall()
-    #     result = ''.join(map(str, chain.from_iterable(list)))
-    #     print('get_score')
-    #     return result
-    #
-    # def add_date(self, id, ddate, text):
-    #     with self.connection:
-    #         self.cursor.execute("")
-    #
-    # def check_date(self, dd, mm):
-    #     with self.connection:
-    #         self.cursor.execute("select")
